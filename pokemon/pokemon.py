f30e3a19889082dba5302560b6046fa75381a6b7
BuildTools <unconfigured@null.spigotmc.org>
2023-11-12 23:12:44 +0100

    Réogranisation du code : - Fonction pour lire le csv et créer les objets (ferme le fichier après utilisation) - Fonction qui gère le combat - Rename de quelque variables - Selection aléatoire avec choice et importe que les fonctions nécessaires - Changement du add pour "plus de clareté"

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -1,58 +1,72 @@
-from random import*
-class Pokemon :
-    def __init__(self, nom, energie, attaque, defense):
-        self.n = nom
-        self.E = energie
-        self.A = attaque
-        self.D = defense
-        
-    def __str__(self):
-        return "{}-{}-{}-{}".format(self.n,self.E,self.A,self.D)
-        
-    def __add__(self,AD):
-
-        de = randint(0,6)
-        print("dé - ",de)
-        
-        if de%2 == 0:
-            assaut = self.A + de
-        else:
-            assaut = self.A - de
-            
-        if assaut > AD.D:
-            dommages = assaut - AD.D
-            AD.E = AD.E - dommages
-            print("domages - ",dommages)
-            print(Pokemon1)
-            print(Pokemon2)
- 
+from random import randint, choice
 import csv
 
-chemin = str(input("veuillez renseigner le chemin du fichier csv: "))
-CHEMINVERSFICHIER = chemin
-f = open( CHEMINVERSFICHIER + "pokemon.csv", encoding="utf8") 
-csv_reader = csv.reader(f,delimiter=';')
+# Classe Pokemon
+class Pokemon:
+    """
+    Classe contenant les stats du Pokémon, l'affichage des stats et la mécanique d'attaque.
+    """
+    def __init__(self, nom, energie, attaque, defense):
+        self.nom = nom
+        self.energie = energie
+        self.attaque = attaque
+        self.defense = defense
 
-proba_1=randint(1,721)
-proba_2=randint(1,721)
+    def __str__(self):
+        return "{}-{}-{}-{}".format(self.nom, self.energie, self.attaque, self.defense)
 
-for row in csv_reader:
-    if row[0] != "#" and int(row[0])==proba_1 :
-        Pokemon1 = Pokemon(str(row[1]),int(row[5]),int(row[6]),int(row[7]))
-    elif row[0] != "#" and int(row[0])==proba_2 :
-        Pokemon2 = Pokemon(str(row[1]),int(row[5]),int(row[6]),int(row[7]))
-   
-for i in range (100):
-    if i%2==0:
-        if Pokemon2.E > 0 and Pokemon1.E > 0:
-            print(Pokemon1 + Pokemon2)
-        else :
-            print("le combat est fini, le gagnant est ",Pokemon2.n)
+    def attaquer(self, adversaire):
+        de = randint(0, 6)
+        print("dé - ", de)
+        
+        assaut = self.attaque + de if de % 2 == 0 else self.attaque - de
+        if assaut > adversaire.defense:
+            dommages = assaut - adversaire.defense
+            adversaire.energie -= dommages
+            print("dommages - ", dommages)
+ 
+# Fonction pour lire les Pokémon depuis un fichier CSV
+def lire_pokemons(chemin_fichier):
+    """
+    Lit un fichier et crée les Pokémon à partir du CSV.
+    Param : Le chemin vers le fichier.
+    Return : Une liste contenant tous les Pokémon.
+    """
+    pokemons = []
+    with open(chemin_fichier, encoding="utf8") as f:
+        csv_reader = csv.reader(f, delimiter=';')
+        for row in csv_reader:
+            if row[0] != "#" and row[0].isdigit():
+                pokemons.append(Pokemon(row[1], int(row[5]), int(row[6]), int(row[7])))
+    return pokemons
+
+# Fonction pour simuler un combat entre deux Pokémon
+def combat(pokemon1, pokemon2):
+    """
+    Simule le combat et annonce le vainqueur.
+    Param : Les deux Pokémon (objets).
+    Return : None.
+    """
+    for i in range(100):
+        if pokemon1.energie <= 0 or pokemon2.energie <= 0:
             break
-    else:
-        if Pokemon2.E > 0 and Pokemon1.E > 0:
-            print(Pokemon2 + Pokemon1)
-        else :
-            print("le combat est fini, le gagnant est ",Pokemon1.n)
-            break
+        attaquant = pokemon1 if i % 2 == 0 else pokemon2
+        defenseur = pokemon2 if i % 2 == 0 else pokemon1
+        attaquant.attaquer(defenseur)
 
+    gagnant = pokemon1.nom if pokemon1.energie > 0 else pokemon2.nom
+    print("Le combat est fini, le gagnant est", gagnant)
+
+
+# Lecture du fichier et création des Pokémon
+chemin = input("Veuillez renseigner le chemin du fichier csv: ")
+pokemons = lire_pokemons(chemin + "pokemon.csv")
+
+# Choix des deux Pokémon
+pokemon1 = choice(pokemons)
+pokemon2 = choice(pokemons)
+while pokemon1 == pokemon2:  # Empêche que les Pokémon soient les mêmes
+    pokemon2 = choice(pokemons)
+
+# Combat
+combat(pokemon1, pokemon2)
